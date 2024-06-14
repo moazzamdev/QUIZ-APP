76b19c083ae2115ed7648a8e0804968e9cec66bb
Moazzam Riaz <126562859+moazzamdev@users.noreply.github.com>
2024-06-14 16:14:56 +0500

    Update constants.dart

--- lib/ui/posts/sidebar/constants.dart
+++ lib/ui/posts/sidebar/constants.dart
@@ -1,6 +1,6 @@
 class Constants {
   static final String BASE_URL = 'https://fcm.googleapis.com/fcm/send';
   static final String KEY_SERVER =
-      'AAAAQMYZtgs:APA91bHYye9dAsnS3O1H_u-yOedv5VAhar3GSLHenVFog8yOyMQU32CqsbCW2_UL7NUDNyjUKZDXzoevXM48ZRB4IWuugM5xdXgLaMcd8bHzDBHxVW8GYvxWfcXQYQ-Raze8H5PLROvL';
+      'FIREBASE_API_KEY';
   static final String SENDER_ID = '278201480715';
 }
